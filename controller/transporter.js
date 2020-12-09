aa1929d8c86eaab9ee9ee443aa284a58c06d1945
surya71 <surya7102000@gmail.com>
2020-12-09 15:29:14 +0530

    add transporter register

--- controller/transporter.js
+++ controller/transporter.js
@@ -9,5 +9,8 @@ class Transporter{
     static login(req,res,err){
         res.sendFile(path.join(process.cwd(),"pages/transporterLogin.html"));
     }
+    static register(req,res,err){
+        res.sendFile(path.join(process.cwd(),"pages/transporterRegister.html"));
+    }
 }
 module.exports = {Transporter}  
--- router/transporter.js
+++ router/transporter.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router()
 const {Transporter}=require ('../controller/transporter')
 router.get("/transporter/login",Transporter.login)
+router.get("/transporter/login",Transporter.register);
 module.exports = router;
